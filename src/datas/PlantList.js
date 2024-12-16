0baedc39613c68c797ce580f2d960b02e4dc129b
Falinantenaina <nantenainaranaivojaona96@gmail.com>
2024-12-16 19:47:03 +0300

    Ajout des données de liste de plantes

---
+++ src/datas/PlantList.js
@@ -0,0 +1,93 @@
+import monstera from "../assets/monstera.jpg"
+import basil from "../assets/basil.jpg"
+import cactus from "../assets/cactus.jpg"
+import calathea from "../assets/calathea.jpg"
+import lyrata from "../assets/lyrata.jpg"
+import mint from "../assets/mint.jpg"
+import olivier from "../assets/olivier.jpg"
+import pothos from "../assets/pothos.jpg"
+import succulent from "../assets/succulent.jpg"
+
+export const plantList = [
+    {
+        id: 1,
+        name: "Monstera",
+        cover: monstera,
+        water: 3,
+        sun: 2,
+        price: 15,
+        category: "Classique"
+    },
+    {
+        id: 2,
+        name: "Ficus Lyrata",
+        cover: lyrata,
+        water: 1,
+        sun: 3,
+        price: 16,
+        category: "Classique"
+    },
+    {
+        id: 3,
+        name: "Pothos Argenté",
+        cover: pothos,
+        water: 2,
+        sun: 1,
+        price: 9,
+        category: "Classique"
+    },
+    {
+        id: 4,
+        name: "Calathea",
+        cover: calathea,
+        water: 3,
+        sun: 2,
+        price: 20,
+        category: "Classique"
+    },
+    {
+        id: 5,
+        name: "Olivier",
+        cover: olivier,
+        water: 1,
+        sun: 3,
+        price: 25,
+        category: "Extérieur"
+    },
+    {
+        id: 6,
+        name: "Cactus",
+        cover: cactus,
+        water: 1,
+        sun: 2,
+        price: 6,
+        category: "Plante grasse"
+    },
+    {
+        id: 7,
+        name: "Basilique",
+        cover: basil,
+        water: 3,
+        sun: 2,
+        price: 5,
+        category: "Extérieur"
+    },
+    {
+        id: 8,
+        name: "Succulente",
+        cover: succulent,
+        water: 1,
+        sun: 2,
+        price: 8,
+        category: "Plante grasse"
+    },
+    {
+        id: 9,
+        name: "Menthe",
+        cover: mint,
+        water: 2,
+        sun: 2,
+        price: 4,
+        category: "Extérieur"
+    }
+]
