9386d7474a3b5e2f1ca050363da19750e9bc95e2
Falinantenaina <nantenainaranaivojaona96@gmail.com>
2024-12-19 20:25:47 +0300

    Ajout de la fonctionnalité filtre par catégorie

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,18 +1,27 @@
 import { plantList } from "../datas/PlantList"
 import PlantItem from "./PlantItem"
 import "../styles/ShoppingList.css"
-const ShoppingList = ({cart, updateCart}) => {
+import Categories from "./Categories"
+import { useState } from "react"
+const ShoppingList = ({ cart, updateCart }) => {
+
+    const [groupedBy, setGroupedBy] = useState("")
 
     const categories = plantList.reduce((acc, plant) =>
         acc.includes(plant.category) ? acc : acc.concat(plant.category),
         []
     )
-    
-    return <ul className="shopping-list">
-        {plantList.map(({id, name, cover, price}) => 
-            <PlantItem key={id} id={id} name={name} price={price} cover={cover} cart={cart} updateCart={updateCart} />
-        )}
-    </ul>
+
+    const plantToShow = groupedBy ? plantList.filter(plant => plant.category === groupedBy) : plantList
+
+    return <>
+        <Categories categories={categories} setGroupedBy={setGroupedBy} />
+        <ul className="shopping-list">
+            {plantToShow.map(({ id, name, cover, price }) =>
+                <PlantItem key={id} id={id} name={name} price={price} cover={cover} cart={cart} updateCart={updateCart} />
+            )}
+        </ul>
+    </>
 
 
 }
